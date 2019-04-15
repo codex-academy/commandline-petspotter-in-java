2b28fb848be754fafef99835b56fd9afc99b00c4
avermeulen <andre.vermeulen@gmail.com>
2019-04-15 13:20:07 +0200

    added JDBC support

--- src/main/java/co/projectcodex/hashmaps/HashmapApp.java
+++ src/main/java/co/projectcodex/hashmaps/HashmapApp.java
@@ -1,5 +1,6 @@
 package co.projectcodex.hashmaps;
 
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -13,7 +14,90 @@ enum Pet {
     goldfish
 }
 
-class PetSpotter {
+interface PetSpotter {
+    void spottedAtPet(Pet petSpotted);
+    int totalPetsSpotted(Pet petType);
+}
+
+class JdbcPetSpotter implements PetSpotter {
+
+    final String INSERT_PET_SQL = "insert into pet_spot_count (pet_type, pet_count) values(?, ?)";
+
+    final String FIND_PET_TYPE_SQL = "select pet_count from pet_spot_count where pet_type = ?";
+
+    final String UPDATE_PET_TYPE_SQL = "update pet_spot_count set pet_count = ? where pet_type = ?";
+
+    Connection conn;
+    PreparedStatement psCreateNewPetSpotted;
+    PreparedStatement psFindPetCount;
+    PreparedStatement psUpdatePetCount;
+
+    public JdbcPetSpotter() {
+        try {
+            conn = DriverManager.
+                    getConnection("jdbc:h2:./db/pet_spotter", "sa", "");
+             psCreateNewPetSpotted = conn.prepareStatement(INSERT_PET_SQL);
+             psFindPetCount = conn.prepareStatement(FIND_PET_TYPE_SQL);
+             psUpdatePetCount = conn.prepareStatement(UPDATE_PET_TYPE_SQL);
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void spottedAtPet(Pet petSpotted) {
+        // insert / update the pet count for the given pet in the database...
+
+        try {
+
+            // find the counter for the current petType;
+
+            psFindPetCount.setString(1, petSpotted.toString());
+            ResultSet rsPetSpotted = psFindPetCount.executeQuery();
+
+            if (!rsPetSpotted.next()) {
+                // System.out.println("no count for " + petSpotted);
+                // insert
+                psCreateNewPetSpotted.setString(1, petSpotted.toString());
+                psCreateNewPetSpotted.setInt(2, 1);
+                System.out.println(psCreateNewPetSpotted.execute());
+
+            } else {
+                int petCount = rsPetSpotted.getInt("pet_count");
+                psUpdatePetCount.setInt(1, ++petCount);
+                psUpdatePetCount.setString(2, petSpotted.toString());
+                psUpdatePetCount.execute();
+            }
+
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+
+
+        // insert
+        // or
+        // update
+    }
+
+    public int totalPetsSpotted(Pet petType) {
+        // get the pet type count from the database - using a sql query...
+        // select using count() where using petType
+
+        try {
+            psFindPetCount.setString(1, petType.toString());
+            ResultSet rs = psFindPetCount.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("pet_count");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        // if this pet wasn't greeted yet
+        return 0;
+    }
+}
+
+class PetSpotterUsingMap implements PetSpotter {
 
     Map<Pet, Integer> petsSpottedMap = new HashMap<Pet, Integer>();
 
@@ -63,7 +147,10 @@ public class HashmapApp {
     public static void main(String[] args) {
 
 
-        PetSpotter petSpotter = new PetSpotter();
+        PetSpotter petSpotter = new JdbcPetSpotter();
+
+        // not changes below this line!!!
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
